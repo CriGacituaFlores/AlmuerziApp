9d6318ab2a01cc14c1bc40e70606e9c0667f8b2e
Cristian <cgacitua@acid.cl>
2020-02-12 22:28:11 -0300

    Create Meals list view

--- screens/Meals.js
+++ screens/Meals.js
@@ -1,10 +1,45 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, StyleSheet, FlatList } from 'react-native';
+import ListItem from '../components/ListItem';
 
-export default () => {
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start'
+  },
+  list: {
+    alignSelf: 'stretch',
+
+  }
+})
+
+const data = [
+  {
+    _id: 'lala', name: 'churrasco', desc: 'Plato tipico, palta, mayonesa'
+  }
+]
+
+const Meals = ({navigation}) => {
   return(
-    <View>
-      <Text>Soy Meal</Text>
+    <View style={styles.container}>
+      <FlatList
+        style={styles.list}
+        data={data}
+        keyExtractor={x => x._id}
+        renderItem={({item}) =>
+          <ListItem 
+            onPress={() => navigation.navigate('Modal', { _id: item._id })}
+            name={item.name}
+          />}
+      />
     </View>
   )
 }
+
+Meals.navigationOptions = ({
+  title: 'Comidas disponibles',
+})
+
+export default Meals;
