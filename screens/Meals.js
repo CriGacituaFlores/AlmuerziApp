928dc966ff04403b5323bb9df19650e143d41fa8
Cristian <cgacitua@acid.cl>
2020-02-20 22:19:33 -0300

    use react hooks for get data

--- screens/Meals.js
+++ screens/Meals.js
@@ -1,45 +1,47 @@
-import React from 'react';
-import { View, StyleSheet, FlatList } from 'react-native';
-import ListItem from '../components/ListItem';
+import React from "react";
+import { View, StyleSheet, FlatList, Text } from "react-native";
+import ListItem from "../components/ListItem";
+import useFetch from "../hooks/useFetch";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'flex-start',
-    justifyContent: 'flex-start'
+    backgroundColor: "#fff",
+    alignItems: "flex-start",
+    justifyContent: "flex-start"
   },
   list: {
-    alignSelf: 'stretch',
-
+    alignSelf: "stretch"
   }
-})
+});
 
-const data = [
-  {
-    _id: 'lala', name: 'churrasco', desc: 'Plato tipico, palta, mayonesa'
-  }
-]
-
-const Meals = ({navigation}) => {
-  return(
+const Meals = ({ navigation }) => {
+  const { loading, data: meals } = useFetch(
+    "https://serverless.crigacituaflores.now.sh/api/meals"
+  );
+  return (
     <View style={styles.container}>
-      <FlatList
-        style={styles.list}
-        data={data}
-        keyExtractor={x => x._id}
-        renderItem={({item}) =>
-          <ListItem 
-            onPress={() => navigation.navigate('Modal', { _id: item._id })}
-            name={item.name}
-          />}
-      />
+      {loading ? (
+        <Text>Cargando...</Text>
+      ) : (
+        <FlatList
+          style={styles.list}
+          data={meals}
+          keyExtractor={x => x._id}
+          renderItem={({ item }) => (
+            <ListItem
+              onPress={() => navigation.navigate("Modal", { _id: item._id })}
+              name={item.name}
+            />
+          )}
+        />
+      )}
     </View>
-  )
-}
+  );
+};
 
-Meals.navigationOptions = ({
-  title: 'Comidas disponibles',
-})
+Meals.navigationOptions = {
+  title: "Comidas disponibles"
+};
 
 export default Meals;
