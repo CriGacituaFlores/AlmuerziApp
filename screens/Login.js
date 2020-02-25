b7967ab2b53c27e17253f49dae7408d06c42048b
Cristian <cgacitua@acid.cl>
2020-02-24 23:57:52 -0300

    custom hook

--- screens/Login.js
+++ screens/Login.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Text, TextInput, View, StyleSheet, Button } from "react-native";
+import useForm from "../hooks/useForm";
 
 const styles = StyleSheet.create({
   title: {
@@ -24,12 +25,31 @@ const styles = StyleSheet.create({
 });
 
 export default ({ navigation }) => {
+  const initialState = {
+    email: "",
+    password: ""
+  };
+  const onSubmit = values => {
+    console.log(values);
+  };
+  const { subscribe, handleSubmit, inputs } = useForm(initialState, onSubmit);
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Iniciar Sesión</Text>
-      <TextInput style={styles.input} placeholder="Email" />
-      <TextInput style={styles.input} placeholder="Password" />
-      <Button title="Iniciar sesión" onPress={() => {}} />
+      <TextInput
+        value={inputs.email}
+        onChangeText={subscribe("email")}
+        style={styles.input}
+        placeholder="Email"
+      />
+      <TextInput
+        value={inputs.password}
+        onChangeText={subscribe("password")}
+        style={styles.input}
+        placeholder="Password"
+        secureTextEntry={true}
+      />
+      <Button title="Iniciar sesión" onPress={handleSubmit} />
       <Button
         title="Registrarse"
         onPress={() => navigation.navigate("Register")}
