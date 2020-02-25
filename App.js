3a0aee73ff3c1cf4968414f9d353a3f28576bf33
Cristian <cgacitua@acid.cl>
2020-02-24 22:45:43 -0300

    login form

--- App.js
+++ App.js
@@ -1,22 +1,50 @@
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
-import MealsScreen from './screens/Meals';
-import Modal from './screens/Modal';
+import { createAppContainer, createSwitchNavigator } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import MealsScreen from "./screens/Meals";
+import LoginScreen from "./screens/Login";
+import RegisterScreen from "./screens/Register";
+import Modal from "./screens/Modal";
 
-const AppNavigator = createStackNavigator({
-  Meals: {
-    screen: MealsScreen
+const OnBoardingNavigator = createStackNavigator(
+  {
+    Login: LoginScreen,
+    Register: RegisterScreen
+  },
+  {
+    initialRouteName: "Login"
   }
-}, {
-  initialRouteName: 'Meals'
-})
+);
 
-const RootStack = createStackNavigator({
-  Main: AppNavigator,
-  Modal: Modal
-}, {
-  mode: 'modal',
-  headerMode: 'none'
-})
+const AppNavigator = createStackNavigator(
+  {
+    Meals: {
+      screen: MealsScreen
+    }
+  },
+  {
+    initialRouteName: "Meals"
+  }
+);
 
-export default createAppContainer(RootStack);
+const RootStack = createStackNavigator(
+  {
+    Main: AppNavigator,
+    Modal: Modal
+  },
+  {
+    mode: "modal",
+    headerMode: "none"
+  }
+);
+
+const baseStack = createSwitchNavigator(
+  {
+    OnBoarding: OnBoardingNavigator,
+    Root: RootStack
+  },
+  {
+    initialRouteName: "OnBoarding"
+  }
+);
+
+export default createAppContainer(baseStack);
---
+++ screens/Login.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Text, TextInput, View, StyleSheet, Button } from "react-native";
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 24,
+    marginBottom: 16
+  },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingHorizontal: 15
+  },
+  input: {
+    height: 40,
+    borderColor: "#ccc",
+    borderWidth: 1,
+    alignSelf: "stretch",
+    marginBottom: 10,
+    paddingHorizontal: 5
+  }
+});
+
+export default ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Iniciar Sesión</Text>
+      <TextInput style={styles.input} />
+      <TextInput style={styles.input} />
+      <Button title="Iniciar sesión" onPress={() => {}} />
+      <Button
+        title="Registrarse"
+        onPress={() => navigation.navigate("Register")}
+      />
+    </View>
+  );
+};
